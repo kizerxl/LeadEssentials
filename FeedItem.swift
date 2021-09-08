6d0ef39b344864e6cd3e4c3dd1154a020bc5245e
Felix <changoof@gmail.com>
2021-09-08 13:22:41 -0400

    Create the FeedItem model

---
+++ FeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Felix Changoo on 9/8/21.
+//
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
